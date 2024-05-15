aafafc25d17f655437fe112083cc7d60fc9f69c1
Armando Di Santo <armando.di.santo@hotmail.com>
2024-05-15 23:18:49 +0200

    switch

---
+++ switch/main.js
@@ -0,0 +1,29 @@
+let temp = prompt("Inserisci temperatura");
+
+switch (true) {
+    case temp < 20:
+        console.log("non ci sono piu' le mezze stagioni");
+        break;
+
+    case temp >= 30:
+        console.log("lu mare, lu sole, lu ientu");
+        break;
+
+    case temp < 30:
+        console.log("mi dia una peroni sudata");
+        break;
+
+    case temp < 0:
+        console.log("non e' tanto il freddo quanto l'umidità");
+        break;
+
+    case temp < -10:
+        console.log("copriti…ancora ti raffreddi");
+        break;
+
+
+
+    default:
+        console.log("Temperatura non valida");
+        break;
+}
